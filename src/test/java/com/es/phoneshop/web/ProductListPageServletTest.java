e93ecf1e8e0de76ec454b2ac8cbe678b714a89f4
Kostyukovich_V_A <demon4ik228228@gmail.com>
2021-09-07 23:06:47 +0300

    Task 1.6 Use ProductDao on PLP - injected productDao via @Mock, created productDao.findProducts() test

--- src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
+++ src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
@@ -1,8 +1,11 @@
 package com.es.phoneshop.web;
 
+import com.es.phoneshop.model.product.ArrayListProductDao;
+import com.es.phoneshop.model.product.Product;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
@@ -11,8 +14,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.mockito.ArgumentMatchers.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -24,8 +32,10 @@ public class ProductListPageServletTest {
     private HttpServletResponse response;
     @Mock
     private RequestDispatcher requestDispatcher;
-
-    private final ProductListPageServlet servlet = new ProductListPageServlet();
+    @Mock
+    private ArrayListProductDao productDao;
+    @InjectMocks
+    private ProductListPageServlet servlet;
 
     @Before
     public void setup(){
@@ -40,4 +50,18 @@ public class ProductListPageServletTest {
         verify(requestDispatcher).forward(request, response);
         verify(request).setAttribute(eq("products"),any());
     }
+
+    @Test
+    public void testProductDaoFindProducts() {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product(-5L, null, null, null, null, 0, null));
+
+        when(productDao.findProducts()).thenReturn(products);
+
+        long expected = -5L;
+        long result = productDao.findProducts().get(0).getId();
+
+        assertEquals(expected, result);
+    }
+
 }
