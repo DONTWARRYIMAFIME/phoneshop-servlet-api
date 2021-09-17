11a3a443beb64e16c91a04f941994aba5c8210ea
Kostyukovich_V_A <demon4ik228228@gmail.com>
2021-09-18 02:15:27 +0300

    Task 3.3: HttpSessionCartService - refactored CartService to store cart in HTTP session

--- src/main/java/com/es/phoneshop/model/product/cart/DefaultCartService.java
+++ src/main/java/com/es/phoneshop/model/product/cart/DefaultCartService.java
@@ -4,11 +4,12 @@ import com.es.phoneshop.exception.IllegalProductQuantityException;
 import com.es.phoneshop.exception.OutOfStockException;
 import com.es.phoneshop.model.product.Product;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class DefaultCartService implements CartService {
-
+    private static final String CART_SESSION_ATTRIBUTE = DefaultCartService.class.getName() + ".cart";
     private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock(true);
     private final Lock writeLock = lock.writeLock();
 
@@ -23,12 +24,16 @@ public class DefaultCartService implements CartService {
     }
 
     @Override
-    public Cart getCart() {
-        writeLock.lock();
+    public Cart getCart(HttpServletRequest request) {
+        lock.writeLock().lock();
         try {
-            return new Cart();
+            Cart cart = (Cart) request.getSession().getAttribute(CART_SESSION_ATTRIBUTE);
+            if (cart == null) {
+                request.getSession().setAttribute(CART_SESSION_ATTRIBUTE, cart = new Cart());
+            }
+            return cart;
         } finally {
-            writeLock.unlock();
+            lock.writeLock().unlock();
         }
     }
 
